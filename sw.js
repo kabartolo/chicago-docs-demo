c848da9968919b6d57762ae7ebf1559cdec65fdd
kabartolo <adairkate@gmail.com>
2020-12-02 21:14:55 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8d75308c8d11725ec77a.js"
+    "url": "webpack-runtime-fff463d6a478d1bda383.js"
   },
   {
     "url": "styles.599224586f67484e0722.css"
@@ -36,10 +36,10 @@ self.__precacheManifest = [
     "url": "styles-407fe62976dc5310c43e.js"
   },
   {
-    "url": "framework-62486be4e7163c2939b7.js"
+    "url": "framework-a882ea00be054f4407c4.js"
   },
   {
-    "url": "c6802b5e-52d310d11cad9a36c215.js"
+    "url": "c6802b5e-b2934afc45338b06aa59.js"
   },
   {
     "url": "9f92645c-ee06cee2b0e6da3da138.js"
@@ -51,18 +51,18 @@ self.__precacheManifest = [
     "url": "a1bc03cf-f91182491ff25c8b714e.js"
   },
   {
-    "url": "app-cf8ea50c6506cc8a9ce8.js"
+    "url": "app-46cd2a7592ba314769e1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c14d87157357d31cdd0616c9c801a355"
+    "revision": "8d6b1fa59e45172e5bda94e91e48896c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8e03ed25f55e17579bf6.js"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "8f0f006493305836055790ddeb5e9730"
+    "revision": "4aecab3c174b2b1ae1d14d4a76330d83"
   },
   {
     "url": "page-data/sq/d/1013078630.json",
@@ -73,16 +73,16 @@ self.__precacheManifest = [
     "revision": "4e549fdfe9c338d7c5a91436f8f3b4bf"
   },
   {
-    "url": "page-data/sq/d/2853624303.json",
-    "revision": "5bc4c78e1bd8b76eb3ef528ad2e4f6bc"
+    "url": "page-data/sq/d/2135897355.json",
+    "revision": "c0b31cf6055f1d650ed1be1596ba400b"
   },
   {
     "url": "page-data/sq/d/3216105679.json",
-    "revision": "ecf302c7651aa11c56bfe8fde105399c"
+    "revision": "654d8b412b85cce75f31dac42c42e169"
   },
   {
     "url": "page-data/sq/d/3235971795.json",
-    "revision": "6a4132a3a3f991ba4f34093d3e5556b6"
+    "revision": "240c3c21c76f4dfea457316e3b6947a8"
   },
   {
     "url": "page-data/sq/d/3883998633.json",
@@ -90,14 +90,14 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/727480955.json",
-    "revision": "de9db29cc9139a67e8b7679659fe1d56"
+    "revision": "38f21f24ecb0c3326e4b8680869307a5"
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "902428750616cc9f8845714305f3f7e8"
+    "revision": "19ac6908ab0b0b7a9d727a03734566c2"
   },
   {
-    "url": "polyfill-1db93d3bf1625938f8c3.js"
+    "url": "polyfill-a02033c5d790f660842d.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -193,7 +193,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/chicago-docs-demo/app-cf8ea50c6506cc8a9ce8.js`))) {
+  if (!resources || !(await caches.match(`/chicago-docs-demo/app-46cd2a7592ba314769e1.js`))) {
     return await fetch(event.request)
   }
 
